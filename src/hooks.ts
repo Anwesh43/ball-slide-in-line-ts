d4a863531c3e2934b5de0c57c5311289e96ff448
Anwesh43 <anweshthecool0@gmail.com>
2021-04-10 22:04:33 +0530

    adding useStyle hook

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,10 +1,15 @@
 import {
     useState, 
-    useEffect
+    useEffect,
+    CSSProperties
 } from 'react'
 
 const scGap : number = 0.02 
 const delay : number = 20 
+
+const maxScale : Function = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
+const divideScale : Function = (scale : number, i : number, n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n 
+const sinify : Function = (scale : number) : number =>  Math.sin(scale * Math.PI)
 
 export const useAnimatedScale = () => {
     const [scale, setScale] : [number, Function] = useState(0)
@@ -38,11 +43,54 @@ export const useDimension = () => {
             setH(window.innerHeight)           
         } 
         return () => {
-            
+
         }
     })
     return {
         w, 
         h 
+    }
+}
+
+export const useStyle = (w : number, h : number, scale : number) => {
+    const size : number = Math.min(w, h) / 6 
+    const lSize : number = Math.min(w, h) / 90
+    const position = 'absolute'
+    const sf = sinify(scale)
+    const sf1 = divideScale(sf, 0, 2)
+    const sf2 = divideScale(sf, 1, 2)
+
+    return {
+        circleStyle() : CSSProperties {
+            const width : string = `${size}px`
+            const height : string = `${size}px`
+            const left : string = `${(w - size) * sf2}px`
+            const top : string = `${h / 2 - size}px`
+            const background : string = 'green'
+            return {
+                position, 
+                width, 
+                height, 
+                left, 
+                top, 
+                background 
+            }
+        },
+
+        lineStyle() : CSSProperties {
+            const width : string = `${w * sf1}px`
+            const height : string = `${lSize / 2}px`
+            const background : string = "green"
+            const left = '0px'
+            const top = `${h / 2}px`
+            return {
+                position, 
+                width,
+                height, 
+                top, 
+                left,
+                background 
+            }
+        }
     }
 }
